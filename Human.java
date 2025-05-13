9e40de37523f2826e6ddd6fbbb9debb38ab1e8aa
Ashishadk9 <aashishadk82@gmail.com>
2025-05-13 06:22:57 +0545

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,7 @@
+public class Human implements LivingBeing
+{
+    @Override
+    public void specialFeature(){
+        System.out.println("Human have the ability to think critically and communicate using complex language.");
+    }
+}
---
+++ LivingBeing.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class LivingBeing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface LivingBeing
+{
+    void specialFeature();
+}
